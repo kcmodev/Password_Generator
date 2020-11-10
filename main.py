ad1b4f1930386e0fb919559580a1d864d7c9d03c
kcmodev <shchristensen3@gmail.com>
2020-11-09 22:05:20 -0600

    added error handling, cleaned up, added comments

--- main.py
+++ main.py
@@ -7,13 +7,22 @@ password = ""
 letters = string.ascii_letters
 symbols = string.punctuation
 
-# take input for user to select password legnth
-num_chars = int(input("Select the number of characters for your password: "))
+# take input for user to select password length and cast to integer
+while True:
+    try:
+        num_chars = int(input("Select the number of characters for your password: "))
 
-# decrement until 0 and return a password of random ascii chars of the desired length
-while num_chars >= 0:
-    random_char = letters + symbols
-    password += random_char[random.randint(0, len(random_char))]
-    num_chars -= 1
+        # decrement until 0 and return a password of random ascii chars of the desired length
+        while num_chars >= 0:
+            random_char = letters + symbols
+            password += random_char[random.randint(0, len(random_char))]
+            num_chars -= 1
 
-print(f"Your generated password is: {password}")
+# print generated password
+        print(f"Your generated password is: {password}")
+
+        break
+
+# catch erroneous input
+    except ValueError:
+        print("Needs to be an integer (1, 2, 3 etc..). Longer is better")
