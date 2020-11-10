698d3144c2e80fe558040a6522a3cde828df2776
kcmodev <shchristensen3@gmail.com>
2020-11-10 09:23:50 -0600

    working on clipboard functionality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 import string
-import tkinter
+import subprocess
 
 # init variables and set letters and symbols using the string library
 password = ""
@@ -15,14 +15,17 @@ while True:
         # decrement until 0 and return a password of random ascii chars of the desired length
         while num_chars >= 0:
             random_char = letters + symbols
-            password += random_char[random.randint(0, len(random_char))]
+            password += random_char[random.randint(0, (len(random_char) - 1))]
             num_chars -= 1
 
-# print generated password
-        print(f"Your generated password is: {password}")
+        # print generated password
+        print(f"Your generated password is: {password} and it is copied to your clipboard.")
+
+        # copy generated password to clipboard
+        subprocess.call(f"echo \"{password}\" | pbcopy")
 
         break
 
-# catch erroneous input
+    # catch erroneous input
     except ValueError:
         print("Needs to be an integer (1, 2, 3 etc..). Longer is better")
