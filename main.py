84d5f14cb908054554dfc0f2812740f1b4a72f8f
kcmodev <shchristensen3@gmail.com>
2020-11-11 10:58:14 -0600

    added functionality to add to clipboard automatically, and exclude problematic characters

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import random
 import string
-import subprocess
+import os
 
 # init variables and set letters and symbols using the string library
 password = ""
 letters = string.ascii_letters
 symbols = string.punctuation
+
+# exclude symbols that will cause issues with the terminal command or
+# are commonly not acceptable password characters
+unwanted_symbols = ['\"', '|', '`', '(', ')', '[', ']', '^', '.']
+
+for symbol in unwanted_symbols:
+    symbols = symbols.replace(symbol, '')
 
 # take input for user to select password length and cast to integer
 while True:
@@ -15,14 +22,15 @@ while True:
         # decrement until 0 and return a password of random ascii chars of the desired length
         while num_chars >= 0:
             random_char = letters + symbols
+
             password += random_char[random.randint(0, (len(random_char) - 1))]
             num_chars -= 1
 
         # print generated password
         print(f"Your generated password is: {password} and it is copied to your clipboard.")
 
-        # copy generated password to clipboard
-        subprocess.call(f"echo \"{password}\" | pbcopy")
+        # copy generated password to clipboards
+        os.system(f"echo -E \"{password}\" | pbcopy")
 
         break
 
